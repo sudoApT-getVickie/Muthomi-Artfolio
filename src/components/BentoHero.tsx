0e2ccc7b96845d0bb6bcd1b16294e6aaf4c8e9cf
vonvicky123 <vonvicky123@gmail.com>
2026-05-07 13:00:56 +0000

    "feat: bento box hero and optimized assets"

--- src/components/BentoHero.tsx
+++ src/components/BentoHero.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './BentoHero.css'; // Links the custom CSS
 
 const techStacks = [
--- src/components/Model.tsx
+++
@@ -1,9 +0,0 @@
-
-import { useGLTF } from '@react-three/drei'
-
-export function Model(props: any) {
-  const { scene } = useGLTF('/models/black_tie_1k.glb')
-  return <primitive object={scene} {...props} />
-}
-
-useGLTF.preload('/models/black_tie_1k.glb')
